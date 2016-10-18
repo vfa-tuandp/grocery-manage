55ff0d4c39713d8707a14a92626558f91c91856d
phuocnt0612 <phuocnt0612@gmail.com>
2016-10-18 15:19:12 +0700

    [Migration] Attribute value table

---
+++ database/migrations/2016_10_18_081802_create_attribute_values_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_attributes_id');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attribute_values');
+    }
+}
