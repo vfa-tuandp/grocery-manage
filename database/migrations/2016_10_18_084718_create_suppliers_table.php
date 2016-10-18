eabffef2ec434664f506451fbe367d6b2e64ca24
phuocnt0612 <phuocnt0612@gmail.com>
2016-10-18 15:52:42 +0700

    [Migration] Supplier table

---
+++ database/migrations/2016_10_18_084718_create_suppliers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id');
+            $table->string('name');
+            $table->string('company')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('suppliers');
+    }
+}
