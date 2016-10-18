746dafe749f92845db0b4a88dd034260f4c0bc89
phuocnt0612 <phuocnt0612@gmail.com>
2016-10-18 17:51:43 +0700

    Integrate docker

--- database/migrations/2016_10_18_044258_create_m_item_type_table.php
+++ database/migrations/2016_10_18_044258_create_m_item_type_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMInventoryTypeTable extends Migration
+class CreateMItemTypeTable extends Migration
 {
     /**
      * Run the migrations.
