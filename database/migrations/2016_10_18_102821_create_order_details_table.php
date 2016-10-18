1b4c5123bae53c2dd3a28c884eaa9aff7f27e919
phuocnt0612 <phuocnt0612@gmail.com>
2016-10-18 17:29:22 +0700

    [Migration] Order detail table

---
+++ database/migrations/2016_10_18_102821_create_order_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->integer('item_id');
+            $table->float('quantity');
+            $table->float('price');
+            $table->float('reduction_on_item')->nullable();
+            $table->float('other_cost_on_item')->nullable();
+            $table->text('note_on_item')->nullable();
+            $table->float('sum');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order_details');
+    }
+}
