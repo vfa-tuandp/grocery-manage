31aae9d1ac41e921cb818463d498d78774f2e094
phuocnt0612 <phuocnt0612@gmail.com>
2017-01-16 18:08:54 +0700

    Modify UI

--- public/assets/my-js/order-create.js
+++ public/assets/my-js/order-create.js
@@ -7,7 +7,6 @@ var CreateOrder = function () {
         }
 
 
-
         $(".form_datetime").datetimepicker({
             autoclose: true,
             format: "dd/mm/yyyy - hh:ii",
@@ -22,10 +21,54 @@ var CreateOrder = function () {
     }
 
 
+    var handleTemplate = function () {
+        //define template
+        var template = $('#create-order-form .row.section:first').clone();
+
+        //define counter
+        var sectionsCount = 0;
+
+        $('#addsection').on('click', '.addsection', function() {
+
+            //increment
+            sectionsCount++;
+
+            //loop through each input
+            var section = template.clone().find(':input').each(function(){
+
+                //set id to store the updated section number
+                var newId = this.id + sectionsCount;
+
+                //update for label
+                $(this).prev().attr('for', newId);
+
+                //update id
+                this.id = newId;
+
+            }).end()
+
+            //inject new section
+                .appendTo('#create-order-form');
+            return false;
+        });
+
+        //remove section
+        $('#create-order-form .remove').on('click', '.remove', function() {
+            //fade out section
+            $(this).parent().fadeOut(300, function(){
+                //remove parent element (main section)
+                $(this).parent().parent().empty();
+                return false;
+            });
+            return false;
+        });
+    }
+
     return {
         //main function to initiate the module
         init: function () {
             handleDatetimePicker();
+            handleTemplate();
         }
     };
 
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -58,12 +58,12 @@
         <div class="portlet-body form">
             <form role="form" action="{{ route('item.store') }}" method="POST">
                 {{ csrf_field() }}
-                <div class="form-body">
+                <div class="form-body" id="create-order-form">
                     <div class="row">
                         <div class="col-md-3">
                             <div class="form-group form-md-line-input">
                                 <div class="input-group date form_datetime">
-                                    <input type="text" readonly size="16" class="form-control">
+                                    <input type="text" readonly size="16" class="form-control" name="datetime">
                                     <label for="category">Ngày hóa đơn</label>
 
                                     <span class="input-group-btn">
@@ -73,13 +73,11 @@
                                                 class="fa fa-calendar"></i></button>
                                     </span>
                                 </div>
-
-                                <!-- /input-group -->
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group form-md-line-input">
-                                <select class="form-control select2me" id="category" name="category_id">
+                                <select class="form-control select2me" id="customer_id" name="customer_id">
                                     @foreach ($customers as $customer)
                                         <option value="{{ $customer->id }}">
                                             <b>{{ $customer->name }}</b> ------------ {{ $customer->company }}
@@ -92,9 +90,8 @@
                         <div class="col-md-1">
                             <div class="form-group form-md-line-input">
                                 <div class="md-checkbox md-checkbox-inline">
-                                    <input type="checkbox" id="check_in_stock" name="check_in_stock" class="md-check"
-                                           checked>
-                                    <label for="check_in_stock">
+                                    <input type="checkbox" id="vat" name="vat" class="md-check">
+                                    <label for="vat">
                                         <span></span>
                                         <span class="check"></span>
                                         <span class="box"></span>
@@ -104,10 +101,10 @@
                         </div>
                     </div>
 
-                    <div class="row">
+                    <div class="row section">
                         <div class="col-md-4">
                             <div class="form-group form-md-line-input has-info">
-                                <select class="form-control" id="category" name="category_id">
+                                <select class="form-control" id="category_id" name="category_id[]">
                                     @foreach ($categories as $category)
                                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endforeach
@@ -117,35 +114,39 @@
                         </div>
                         <div class="col-md-4">
                             <div class="form-group form-md-line-input">
-                                <input type="text" class="form-control" id="product_name" name="name"
-                                       placeholder="Nhập vào tên sản phẩm">
+                                <select class="form-control" id="item_id" name="item_id[]">
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
                                 <label for="product_name">Mặt hàng</label>
-                                <span class="help-block">Nhập tên sản phẩm</span>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group form-md-line-input">
-                                <input type="number" class="form-control" id="price_out_hint" name="price_out_hint"
+                                <input type="number" class="form-control" id="price_out" name="price_out[]"
                                        placeholder="Giá bán ra">
-                                <label for="price_out_hint">Giá bán ra</label>
-                                <span class="help-block">Có thể bỏ qua</span>
+                                <label for="price_out_hint">Giá bán</label>
                             </div>
                         </div>
                         <div class="col-md-2">
                             <div class="form-group form-md-line-input">
-                                <input type="text" class="form-control" id="unit" name="unit"
+                                <input type="text" disabled class="form-control" id="unit" name="unit"
                                        placeholder="Đvt">
                                 <label for=unit>Đơn vị tính</label>
                                 <span class="help-block">Đơn vị tính của sản phẩm</span>
                             </div>
+                            <a class="btn btn-circle btn-icon-only red" href="javascript:;">
+                                <i class="icon-trash"></i>
+                            </a>
                         </div>
                     </div>
-                    <div class="form-actions noborder">
-                        <button class="btn blue">Thêm</button>
-                    </div>
-                    <div class="form-actions noborder">
-                        <button type="submit" class="btn blue">Tạo mới</button>
-                    </div>
+                </div>
+                <div id="addsection" class="form-actions noborder">
+                    <a class="btn blue addsection">Thêm</a>
+                </div>
+                <div class="form-actions noborder">
+                    <button type="submit" class="btn blue">Tạo mới</button>
                 </div>
             </form>
         </div>
